cd434aed61980e4a2b3c260282eaa7d28e5a060c
Min <106130309+ming-sil@users.noreply.github.com>
2024-01-08 13:27:10 +0900

    Add files via upload

---
+++ min-board/src/test/java/com/study/minboard/MinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.minboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
